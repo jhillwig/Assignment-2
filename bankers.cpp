2761200b85de097881334944ed26f9e002fce081
jhillwig <jhillwig@kent.edu>
2025-11-23 21:40:48 -0500

    Update bankers.cpp
    Adding comments into bankers.cpp

--- bankers.cpp
+++ bankers.cpp
@@ -9,13 +9,16 @@ bool checkSafeState(vector<vector<int>>& allocMatrix, vector<vector<int>>& maxMa
   vector<bool> finish(numProcs, false);
   
   int numFinished = 0;
+  // Looking for processes who's needs can be met by currently available resources
   while (numFinished < numProcs) {
     bool found = false;
 
+    // Checking each process to see if it can run safely
     for (int i = 0; i < numProcs; ++i) {
       if (!finish[i]) {
         bool canExecute = true;
 
+        // Checking to see if a process can run based off of resources needed vs resources available
         for (int j = 0; j < numRes; ++j) {
           if (maxMatrix[i][j] - allocMatrix[i][j] > work[j]) {
             canExecute = false;
@@ -23,26 +26,29 @@ bool checkSafeState(vector<vector<int>>& allocMatrix, vector<vector<int>>& maxMa
           }
         }
 
+        // Simulate finishing the process if it can run
         if (canExecute) {
           for (int j = 0; j < numRes; ++j) {
             work[j] += allocMatrix[i][j];
           }
 
-          finish[i] = true;
-          found = true;
-          ++numFinished;
+          finish[i] = true;    // Mark as completed
+          found = true;        // Progress was made
+          ++numFinished;       // Counting finished processes
 
           cout << "Executing Process " << i << endl;
         }
       }
     }
 
+    // If the process couldnt run, the system is considered unsafe 
     if (!found) {
       cout << "System is not in a safe state." << endl;
       return false;
     }
   }
-  
+
+  // All processes could run safely
   cout << "System is in a safe state." << endl;
   return true;
   }
@@ -51,34 +57,41 @@ bool checkSafeState(vector<vector<int>>& allocMatrix, vector<vector<int>>& maxMa
 int main() {
   ifstream inputFile("input.txt");
 
+  // Check file open
   if (!inputFile) {
     cerr << "Error opening file." << endl;
     return 1;
   }
 
   int numProcs, numRes;
+  // Reading number of processes and resource types
   inputFile >> numProcs >> numRes;
 
+  // Creating allocation and maximum matrices
   vector<vector<int>> allocMatrix(numProcs, vector<int>(numRes));
   vector<vector<int>> maxMatrix(numProcs, vector<int>(numRes));
 
+  // Read allocation matrix
   for (int i = 0; i < numProcs; i++) {
     for (int j = 0; j < numRes; j++) {
       inputFile >> allocMatrix[i][j];
     }
   }
 
+  // Reading Maximum matrix
   for (int i = 0; i < numProcs; i++) {
     for (int j = 0; j < numRes; j++) {
       inputFile >> maxMatrix[i][j];
     }
   }
 
+  // Reading in available resources
   vector<int> availResources(numRes);
   for (int j = 0; j < numRes; j++) {
     inputFile >> availResources[j];
   }
 
+  // Printing matrices for verification
   cout << "allocation:" << endl;
   for (int i = 0; i < numProcs; i++) {
     for (int j = 0; j < numRes; j++) {
@@ -101,6 +114,7 @@ int main() {
   }
   cout << endl;
 
+  // Run Banker's algortihm
   checkSafeState(allocMatrix, maxMatrix, availResources, numProcs, numRes);
 
   return 0;
